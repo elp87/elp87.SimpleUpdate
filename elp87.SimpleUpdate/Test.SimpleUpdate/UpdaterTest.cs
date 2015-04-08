fbe4d842bcf409ba7a0214a6c5293365cebaeda7
elp87 <elp1987@gmail.com>
2015-04-08 17:09:05 +0300

    Add unit test project

---
+++ elp87.SimpleUpdate/Test.SimpleUpdate/UpdaterTest.cs
@@ -0,0 +1,15 @@
+﻿using elp87.SimpleUpdate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.SimpleUpdate
+{
+    [TestClass]
+    public class UpdaterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Updater upd = new Updater();
+        }
+    }
+}
