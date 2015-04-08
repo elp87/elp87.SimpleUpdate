f689014807392dad9be03fced5b2ef126c9134f6
elp87 <elp1987@gmail.com>
2015-04-08 16:24:50 +0300

    Updater: Add _configFileName

---
+++ elp87.SimpleUpdate/elp87.SimpleUpdate/Updater.cs
@@ -0,0 +1,22 @@
+﻿
+namespace elp87.SimpleUpdate
+{
+    public class Updater
+    {
+        #region Fileds
+        private string _configFileName;
+        #endregion
+
+        #region Constructors
+        public Updater(string configFileName)
+        {
+            this._configFileName = configFileName;
+        }
+
+        public Updater()
+        {
+            this._configFileName = System.AppDomain.CurrentDomain.BaseDirectory + @"\updconfig.xml";
+        }
+        #endregion
+    }
+}
